ffdef8b7fc6e7823030c6dd52484b31f6d34c4a5
john <jbankc@gmail.com>
2020-07-26 20:54:41 +1200

    first

---
+++ StudentSIMS/StudentSIMS/Migrations/20200725110805_Address.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StudentSIMS.Migrations
+{
+    public partial class Address : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                columns: table => new
+                {
+                    studentId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    streetNumber = table.Column<int>(nullable: false),
+                    Street = table.Column<string>(nullable: true),
+                    Suburb = table.Column<string>(nullable: true),
+                    City = table.Column<string>(nullable: true),
+                    Postcode = table.Column<int>(nullable: false),
+                    Country = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.studentId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Addresses");
+        }
+    }
+}
---
+++ StudentSIMS/StudentSIMS/Models/Address.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+// namespace contains the collections of symbols/methods so each is uniquely identified
+// here the class Student belongs to the namespace StudentSIMS.Models, you can use access
+// the Student class from other files withusing StudentSIMS.Models
+namespace StudentSIMS.Models
+{
+    public class Address
+    {
+        // annotate studentId to be the unique identifier of the Model which maps
+        // to the primary key of the database
+        [Key]
+        // DatabaseGenerated can be assigned so that a value of an attribute is automatically generated
+        // in this case, the studentId. DatabaseGeneratedOption.Identity means the value will only be
+        // generated once when the row is first created, and it cannot be updated
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int studentId { get; set; }
+        // firstName is required when adding a Student to the database, and it will have 100 characters max
+        [Required]
+        public int streetNumber { get; set; }
+        public string Street { get; set; }
+        public string Suburb { get; set; }
+        public string City { get; set; }
+        public int Postcode { get; set; }
+
+        public string Country { get; set; }
+    }
+}
